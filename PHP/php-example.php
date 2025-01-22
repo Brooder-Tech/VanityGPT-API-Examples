4843ecea6359a42405b989726680eba049c84134
Andy Dank <159937876+dank-developer@users.noreply.github.com>
2025-01-22 21:37:25 +0100

    Update php-example.php

--- PHP/php-example.php
+++ PHP/php-example.php
@@ -1,4 +1,9 @@
 <?php
+
+// The more complex he api call the longer it takes to create the wallet.  A Standard php execution time in 30 seconds.  
+// So this needs to be extended for more complex calls (The example below sets the execution time to 5 minutes, but this can be inreates to infinite )
+ini_set('max_execution_time', '300');
+
 $url = "https://api.vanityGPT.com/create-address/?text=AA&method=START";
 $headers = [
     "x-api-key: your_api_key",
